742c0ba464c1469cba263944318234ab083cd114
kristina-mmi <kristina@motionmetrics.com>
2020-09-27 22:25:06 -0700

    - clean up

--- userAPI/src/database/postgresClient.ts
+++ userAPI/src/database/postgresClient.ts
@@ -31,12 +31,4 @@ export default class PostgresDatabaseClient implements IDatabaseClient {
             throw new Error(`Failed to insert new record: ${err.message}`);
         }
     }
-
-    // public async setShortenedUrl(id: number, shortenedUrl: string): Promise<void> {
-    //     try {
-    //         await this.pool.query(`UPDATE ${this.tableName} SET shortened_url = $1 WHERE id = $2;`, [shortenedUrl, id]);
-    //     } catch (err) {
-    //         throw new Error(`Failed to update record: ${err.message}`);
-    //     }
-    // }
 }
